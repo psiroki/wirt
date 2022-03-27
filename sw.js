785510adad823f46f2343261fd119843bbe8a821
Peter Siroki <psiroki@@users.noreply.github.com>
2022-03-27 19:55:57 +0200

    PWA tweaks

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-self.addEventListener("fetch", function(event) {
+const cacheName = "wirt-v1";
+const files = [
+  "/wirt/",
+  "/wirt/?source=pwa",
+  "/wirt/crc32.js",
+  "/wirt/drop.js",
+  "/wirt/exif.js",
+  "/wirt/exifWorker.js",
+  "/wirt/imageBoom.js",
+  "/wirt/maskSpinner.svg",
+  "/wirt/solo_jazz_lo.png",
+  "/wirt/manifest.json",
+  "/wirt/icon-192.png",
+  "/wirt/icon-512.png",
+];
+
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
@@ -6,23 +22,10 @@ self.addEventListener("fetch", function(event) {
   );
 });
 
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("wirt").then(cache => {
-      return cache.addAll([
-        "/wirt/",
-        "/wirt/?source=pwa",
-        "/wirt/crc32.js",
-        "/wirt/drop.js",
-        "/wirt/exif.js",
-        "/wirt/exifWorker.js",
-        "/wirt/imageBoom.js",
-        "/wirt/maskSpinner.svg",
-        "/wirt/solo_jazz_lo.png",
-        "/wirt/manifest.json",
-        "/wirt/icon-192.png",
-        "/wirt/icon-512.png",
-      ]);
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(files);
     })
   );
 });
